cd64e4a1e8db97e9ef8f9c2cffd06c155c8aaaa3
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2018-12-15 15:14:25 +0100

    Simpler bug

---
+++ src/main/java/io/github/oliviercailloux/y2018/jsonb_serial/Wrapper.java
@@ -0,0 +1,12 @@
+package io.github.oliviercailloux.y2018.jsonb_serial;
+
+import com.google.common.collect.ImmutableSet;
+
+public class Wrapper {
+	public Wrapper() {
+	}
+
+	public ImmutableSet<String> getData() {
+		return null;
+	}
+}
